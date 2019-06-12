dad4760b3496f3cceb0f8ed0747978c159c0c106
Stella McEwen <mcewen.stella@gmail.com>
2019-06-12 21:36:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "male", "brown", "sweet", 1);
-INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Rowdy", 5, "male", "black", "rowdy", 0);
-INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Tabitha", 2, "female", "white", "fiesty", 1);
-INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Sergeant Brown", 5, "male", "brown", "imposing", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "male", "black", "rowdy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "female", "white", "fiesty", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 5, "male", "brown", "imposing", 1);
 INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Melissa", 3, "female", "black", "indolent", 1);
 INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Grinch", 1, "male", "white", "mean", 1);
 INSERT INTO bears (name, age, gender, temperament, alive) VALUES ("Wendy", 6, "female", "brown", "wise", 1);
